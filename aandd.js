6b9a645f15440034b871e66e03b6f76f9def0726
a8120079 <a8120079@gsuite.si.aoyama.ac.jp>
2022-05-17 19:41:09 +0900

    work

--- aandd.js
+++ aandd.js
@@ -1,14 +1,33 @@
-function show(array) {
-    var html = '';
-    html = html + '<table border="1">';
-    html = html + '<tr>';
-    for (var i = 0; i < array.length; i++) {
-        html = html + '<td>' + array[i] + '</td>';
-    }
+function show(obj) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
         html = html + '</tr>';
         html = html + '</table>';
-        //document.getElementById('res').innerHTML = html;
+        //html = html + '<br>';
         document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
+    }
+}
+
+function createOrderedRandomNumbers(numbers, n) {
+    var count = 0;
+    for (var i = 1; ; i++) {
+        if (Math.floor(Math.random() * 3) == 0) {
+            numbers.push(i);
+            count++;
+            if (count >= n) {
+                break;
+            }
+        }
+    }
 }
 
 function createSequentialNumbers(array, n) {
